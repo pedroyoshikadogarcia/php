c1be5657bdeef5e37872de51ecba5bd538679b33
Pedro Yoshikado Garcia <161722472+pedroyoshikadogarcia@users.noreply.github.com>
2025-02-21 20:27:25 -0300

    Add files via upload

---
+++ media.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php   
+        $n1 = 1;
+        $n2 = 5;
+        $n3 = 8;
+        $n4 = 10;
+
+        echo("A nota é " . ($n1 + $n2 + $n3 + $n4)/4)
+        ?>
+</body>
+</html>
---
+++ raio.php
@@ -0,0 +1,8 @@
+<html>
+    <?php
+
+        $raio = 5;
+
+        echo("A área do circulo é: " . ($raio*$raio) * pi());
+    ?>
+</html>
